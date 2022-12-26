bc8ac0266a977748a4b92bf25ae8403562cab495
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-12-26 22:29:49 +0200

    started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
+import styled from 'styled-components';
+import { Routes, Route, NavLink, Outlet } from 'react-router-dom';
+// import SharedLayout from '../pages/SharedLayout';
+
+import { Home } from 'pages/Home';
+import { Movies } from 'pages/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Header>
+        <NavLinkItem to="/">Home</NavLinkItem>
+        <NavLinkItem to="/movies">Movies</NavLinkItem>
+      </Header>
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="/Movies" element={<Movies />}></Route>
+        </Routes>
+      </Main>
+    </>
   );
 };
+
+const NavLinkItem = styled(NavLink)`
+  margin-right: 30px;
+  margin-left: 30px;
+  text-decoration: none;
+  color: #010101;
+
+  :hover {
+    color: blue;
+  }
+
+  &.active {
+    color: orange;
+  }
+`;
+
+const Header = styled.div`
+  height: 100px;
+  /* position: absolute; */
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  font-size: 40px;
+  color: #010101;
+`;
+
+const Main = styled.main`
+  /* margin-top: 100px; */
+`;
---
+++ src/pages/SharedLayout.jsx
@@ -0,0 +1,7 @@
+// import { NavLink, Outlet } from 'react-router-dom';
+
+// export const SharedLayout = () => {
+//   return (
+
+//   );
+// };
